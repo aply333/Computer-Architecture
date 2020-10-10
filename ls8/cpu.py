4c92dce02252c6940c2ab52ea402026d6ead1aa8
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-10-10 12:27:23 -0700

    SPRINT MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,73 @@
 
 import sys
 
+JNE = 0b01010110
+JEQ = 0b01010101
+CMP = 0b10100111
+JMP = 0b01010100
+ADD = 0b10101000
+RET = 0b00010001
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+POP = 0b01000110
+PUSH = 0b01000101
+CALL = 0b01010000
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0]*8
+        self.ram = [0]*256
+        self.reg[7] = 0xF4
+        self.halted = False
+        self.pc = 0
+        self.SP = 7
+        self.flag = 0b00000000
 
-    def load(self):
-        """Load a program into memory."""
+    def ram_read(self, location):
+        """Return value at given Address"""
+        return self.ram[location]
+    
+    def ram_write(self, location, value):
+        """Write to a location in Memmory"""
+        self.ram[location] = value
 
+    def load(self, filename):
+        """Load a program into memory.""" 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        pg_len = 0
+        with open(filename) as fp:
+            for line in fp:
+                line_split = line.split("#")
+                num = line_split[0].strip()
+                if num == "":
+                    continue
+                val = int(num, 2)
+                self.ram_write(address, val)
+                address += 1
+                pg_len += 1
+        self.program_end = pg_len - 1
+        self.reg
+        
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op =="MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op =="CMP":
+            if reg_a == reg_b:
+                self.flag == 0b00000001
+            elif reg_a < reg_b:
+                self.flag == 0b00000100
+            elif reg_a > reg_b:
+                self.flag == 0b00000010
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +94,69 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.halted != True:
+            instruction_to_execute = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            self.execute_instruction(instruction_to_execute, operand_a, operand_b)
+            
+    def execute_instruction(self, instruction, operand_a, operand_b):
+        if instruction == ADD:
+            self.alu("ADD", operand_a, operand_b)
+            self.pc += 3
+        if instruction == HLT:
+            self.halted = True
+            self.pc += 1
+        elif instruction == LDI:
+            self.reg[operand_a] = operand_b
+            self.pc += 3
+        elif instruction == PRN:
+            print(self.reg[operand_a])
+            self.pc += 2
+        elif instruction == MUL:
+            self.alu("MUL", operand_a, operand_b)
+            self.pc +=3
+        elif instruction == PUSH:
+            self.reg[self.SP] -= 1
+            register_value = self.reg[operand_a]
+            self.ram[self.reg[self.SP]] = register_value
+            self.pc += 2
+        elif instruction == POP:
+            top = self.ram[self.reg[self.SP]]
+            self.reg[operand_a] = top
+            self.reg[self.SP] += 1
+            self.pc += 2
+        elif instruction == CALL:
+            self.reg[self.SP] -= 1
+            self.ram[self.reg[self.SP]] = operand_b
+            self.pc += 2
+        elif instruction == RET:
+            return_address = self.ram[self.reg[self.SP]]
+            self.ram[self.reg[self.SP]] += 1
+            self.pc = return_address
+        elif instruction == CMP:
+            print("CMP CALLED")
+            self.alu("CMP", operand_a, operand_b)
+            self.pc += 3
+        elif instruction == JMP:
+            print("JMP CALLED")
+            self.pc = self.reg[operand_a]
+        elif instruction == JEQ:
+            print("JEQ CALLED")
+            if self.flag == 0b00000001:
+                self.pc == self.reg[operand_a]
+            else:
+                self.pc += 2
+        elif instruction == JNE:
+            print("{0:b}".format(operand_a))
+            print(operand_a)
+            if self.flag == 0b00000000:
+                print("THEY NOT")
+                self.pc = self.reg[operand_a]
+                print("{0:b}".format(self.pc))
+            else:
+                self.pc += 2
+        
+            
+
+
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python3
 
-"""Main."""
+# """Main."""
+
+# import sys
+# from cpu import *
+
+# def main(argv):
+#     if len(argv) != 2:
+#         print("Requires an input")
+#         return 1
+#     cpu = CPU()
+#     cpu.load()
+#     cpu.run()
+#     return 0
+
+
+# if len(sys.argv) != 2:
+#     print("Incorrect Usage")
+#     sys.exit(1)
+    
+# cpu = CPU()
+# cpu.load(sys.argv[1])
+# cpu.run()
+
 
 import sys
 from cpu import *
 
-cpu = CPU()
+def main(argv):
+    """Main."""
+    if len(argv) != 2:
+        print(f"usage: {argv[0]} filename", file=sys.stderr)
+        return 1
 
-cpu.load()
-cpu.run()
+    cpu = CPU()
+
+    cpu.load(argv[1])
+
+    cpu.run()
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
